9bfb317f527b4db23478dd2b3fdcabe1bf7e46e1
Alfredoc <alfredo10396.c@gmail.com>
2025-06-30 18:41:26 -0600

    manage errors in dashboard config get

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -21,30 +21,33 @@ export class DashboardComponent {
   loading: boolean = true; // Flag to indicate loading state
 
   ngOnInit() {
-    // Code to run on component initialization
+    this.getReservationConfig();
+  }
+
+  getReservationConfig() {
+    this.loading = true;
+
     this.reservationApiService.getReservationConfig().subscribe({
-      next: (res: any) => {
-        console.log('Configuration loaded:', res);
-        this.config = res;
-        console.log('Configuration loaded:', this.config.frequency);
-        console.log('Holidays loaded:', this.config.holidays);
-        console.log('Service days loaded:', this.config.serviceDays);
+      next: (response: any) => {
+        console.log('API response:', response);
+
+        // Si la respuesta es válida y tiene frequency
+        if (response && response.frequency != null) {
+          this.config = response;
+        } else {
+          // Si no hay configuración en la BD
+          this.config = { frequency: null, serviceDays: [], holidays: [] };
+        }
+
+        this.loading = false;
       },
-      error: (error: any) => {
-        console.error('Error loading configuration:', error);
-        this.config.frequency = null;
-        this.config.holidays = [];
-        this.config.serviceDays = [];
-        console.log(
-          'No configuration found, using default frequency:',
-          this.config.frequency
-        );
-      },
-      complete: () => {
-        console.log('Configuration loading complete');
-        this.loading = false; // Set loading to false when complete
+      error: (error) => {
+        console.error('Error al obtener configuración:', error);
+
+        // Si hay error en la petición, define config como vacío
+        this.config = { frequency: null, serviceDays: [], holidays: [] };
+        this.loading = false;
       },
     });
-    console.log('Dashboard component initialized');
   }
 }
