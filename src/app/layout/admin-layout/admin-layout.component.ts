e08dd61a3accbbbd226bd6f66840358ec9c751c7
Alfredoc <alfredo10396.c@gmail.com>
2025-10-24 15:23:46 -0600

    logout button delete the acces token form localStorage

--- src/app/layout/admin-layout/admin-layout.component.ts
+++ src/app/layout/admin-layout/admin-layout.component.ts
@@ -7,6 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { NgIf } from '@angular/common';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-admin-layout',
@@ -28,4 +29,21 @@ export class AdminLayoutComponent {
   constructor(
   ) { }
   showMenu = true;
+  logout(){
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: 'Esto cerrará tu sesión.',
+      icon: 'warning',
+      iconColor: '#0a1157ff',
+      showCancelButton: true,
+      confirmButtonText: 'Aceptar',
+      confirmButtonColor: '#0d1013ff',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.removeItem('access_token');
+        window.location.href = '/login';
+      }
+    });
+  }
 }
